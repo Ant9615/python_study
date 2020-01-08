b204c391e03a87e4541bbc880db0dde306f44727
ant9615 <rudals901@gmail.com>
2020-01-08 22:57:06 +0900

    list add

--- ch1/list.py
+++ ch1/list.py
@@ -12,4 +12,58 @@ b_list = ['prin', '5', ['star', 'circle', 9]]
 print("#2: {}".format(b_list))
 # element가 3개인 이유는 리스트 안에 또 다른 리스트가 있기에 하나로 묶어서 하나의 element로 인식함
 print("#2.1: b_list has {} elements".format(len(b_list)))
-print("#2.2: 5 is in b_list {} time".format(b_list.count(5)))  # count는 리스트 내에서 특별한 값을 보여줌
+print("#2.2: 5 is in b_list {} time".format(
+    b_list.count(5)))  # count는 리스트 내에서 특별한 값을 보여줌
+# index
+print("#3: {}".format(a_list[0]))  # 첫번째 리스트 요소
+print("#3.1: {}".format(a_list[2]))
+print("#3.2: {}".format(a_list[-1]))  # 맨 마지막 요소
+print("#3.3: {}".format(a_list[-2]))  # 맨 뒤에서 두번째 요소
+print("#3.4: {}".format(a_list[-3]))
+print("#3.5: {}".format(b_list[1]))
+print("#3.6: {}".format(b_list[-1]))  # 맨 마지막 요소
+
+# separate list
+print("#4: {}".format(a_list[0:3]))  # 요소 모두 출력
+print("#4.1: {}".format(a_list[0:2]))  # 리스트의 첫번째 두번째만 부분집합으로 묶어 출력
+print("#4.2: {}".format(b_list[:2]))  # 리스트의 두번째 요소까지 출력
+print("#4.3: {}".format(b_list[1:]))  # 리스트의 두번째부터 마지막까지 출력
+
+# list copy & merge
+n_list = a_list[:] + b_list[:]  # 기존 두 리스트를 합침
+print("#5: {}".format(n_list))
+
+# in & not in
+a = 2 in a_list
+print("#6: {}".format(a))
+if 2 in a_list:
+    print("#6.1: {}".format(a_list))
+b = 6 not in b_list
+print("#6.2: {}".format(b))
+if 6 not in a_list:
+    print("#6.3:{}".format(a_list))
+
+#append, remove, pop
+a_list.append(4)  # append는 list 마지막에 추가
+for i in range(5, 9):  # 5부터 8까지 추가
+    a_list.append(i)
+print("#7: {}".format(a_list))
+a_list.insert(2, 10) #2 뒤에 10 추가(원하는 자리에 추가)
+print("#7.1: {}".format(a_list))
+a_list.extend(b_list) #a_list에 b_list 추가
+print("#7.2:{}".format(a_list))
+a_list.remove(8) #list에 8제가
+print("#7.3: {}".format(a_list))
+a_list.pop()#list의 마지막 요소 제거
+print("#7.3: {}".format(a_list))
+
+#reverse 
+a_list.reverse() #list 반전
+print("#8: {}".format(a_list))
+
+#sort & sorted 
+u_list = [9,3,6,7,3,6,4,5,1]
+co_list = u_list[:]
+print("#9:{}".format(u_list)) #정렬 전
+u_list.sort() #정렬
+print("9.1:{}".format(u_list)) #정렬 후
